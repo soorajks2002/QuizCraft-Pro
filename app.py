2f40d4cdc686e15a211cfd5603e22619e53cb77e
sooraj <soorajks2002@gmail.com>
2023-08-05 14:04:37 +0530

    updated streamlit ui

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ import streamlit as st
 from llm_api import generate_mcq
 
 st.title("Q-Easy")
- 
-number_of_question = st.slider("Pick number of question to be created", 1, 5)
-content = st.text_area("Enter content")
 
-if st.button("Generate MCQ") :
-    mcqs = generate_mcq(number_of_question, content)
+c1, c2 = st.columns(2)
+number_of_question = c1.slider("Pick number of question to be created", 1, 5)
+difficulty_level = c2.selectbox("Select Difficulty Level", [
+                                "Easy", "Medium", "Hard"])
+content = st.text_area("Enter content", height=200)
+
+if st.button("Generate MCQ"):
+    st.divider()
+    response = generate_mcq(number_of_question, content, difficulty_level)
     
-    for mcq in mcqs : 
-        st.markdown("### {question}".format(question=mcq['question']))
-        for option in mcq["options"] :
-            if option == mcq['answer'] :
-                st.success(option)
-            else :
-                st.markdown("###### {question}".format(question=option))
-        st.divider()
+    if response["result"] :
+        mcqs = response["response"]
+        for mcq in mcqs:
+            st.markdown("### {question}".format(question=mcq['question']))
+            for option in mcq["options"]:
+                if option == mcq['answer']:
+                    st.success(option)
+                else:
+                    st.markdown("###### {question}".format(question=option))
+            st.divider()
+
+    else :
+        st.error(response["response"])
