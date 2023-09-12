862dada51cf1348bb2ad89f1fce071eed5512bb1
sooraj <soorajks2002@gmail.com>
2023-09-12 11:53:59 +0530

    changed UI from centered to wide columns

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 import streamlit as st
 from llm_api import generate_mcq
 
-st.set_page_config(page_title="QuizCraft", page_icon="🎓")
-st.title("QuizCraft Pro 📝")
+st.set_page_config(page_title="QuizCraft", page_icon="🎓", layout='wide')
+input_col, result_col = st.columns(2)
 
-c1, c2 = st.columns(2)
+input_col.title("QuizCraft Pro 📝")
+
+input_col.markdown("##### Enter The Content")
+content = input_col.text_area("Enter content", height=200, label_visibility='collapsed')
+c1, c2 = input_col.columns(2)
 number_of_question = c1.slider("Pick number of question to be created", 1, 5)
 difficulty_level = c2.selectbox("Select Difficulty Level", [
                                 "Easy", "Medium", "Hard"])
-content = st.text_area("Enter content", height=200)
 
-if st.button("Generate MCQ"):
-    st.divider()
-    response = generate_mcq(number_of_question, content, difficulty_level)
-    
-    if response["result"] :
-        mcqs = response["response"]
-        for mcq in mcqs:
-            st.markdown("### {question}".format(question=mcq['question']))
-            for option in mcq["options"]:
-                if option == mcq['answer']:
-                    st.success(option)
-                else:
-                    st.markdown("###### {question}".format(question=option))
-            st.divider()
+if input_col.button("Generate Quiz"):
+    if content :
+        # st.divider()
+        response = generate_mcq(number_of_question, content, difficulty_level)
+        
+        if response["result"] :
+            mcqs = response["response"]
+            for mcq in mcqs:
+                result_col.markdown("#### {question}".format(question=mcq['question']))
+                for option in mcq["options"]:
+                    if option == mcq['answer']:
+                        result_col.success(option)
+                    else:
+                        result_col.markdown("###### {question}".format(question=option))
+                result_col.divider()
+                # result_col.markdown("#####")
 
+        else :
+            result_col.error(response["response"])
     else :
-        st.error(response["response"])
+        input_col.warning("Please Enter Your Content")
